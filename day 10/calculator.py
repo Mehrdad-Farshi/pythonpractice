aa06d397ed6f2375f37fd076677cd4a5f17fbc96
mehrdad <meh1376@hotmail.com>
2022-08-08 11:28:15 +0430

    calculator is done but i couldnt use the recerstion for calculator() function

--- day 10/calculator.py
+++ day 10/calculator.py
@@ -5,7 +5,7 @@ def add (n1,n2):
     # substracte
 def sub(n1,n2):
     '''n2 - n1'''
-    return n2 - n1
+    return n1 - n2
     # multiple
 def multiple(n1,n2):
     '''n1 * n2'''
@@ -21,22 +21,33 @@ opration= {
     '/':devide,
     '*':multiple,
 }
-def calc(num1,num2):
-    func = opration[opration_symb]
-    return func(num1,num2)
-calc_continue =True
-while(calc_continue):
-    num1 = int(input('what the firts number ? :'))
+def calculator():   
+    def calc(num1,num2):
+        func = opration[opration_symb]
+        return func(num1,num2)
+
+    num1 = float(input('what the firts number ? :'))
     for symb in opration :
         print (symb)
-    num2 = int(input('what the sec   number ? :'))
     opration_symb = input('pick an opration from above : ')
-    answear1 = calc(num1,num1)
-    state = input('do you want to continue y or n ? ')
-    if state == 'n' : 
-        calc_continue = False
-    
+    num2 = float(input('what the sec   number ? :'))
+    answear1 = calc(num1,num2)
+    print(f'{num1} {opration_symb} {num2} = {answear1}')
 
-
-
-print(f'{num1} {opration_symb} {num2} = {calc(num1,num2)}')
+    # calc_continue =True
+    while(True):
+        state= input('are you wish to continue ? y or n : ') 
+        if state == 'n' :
+            # calc_continue = False
+            break
+            calculator()
+        else:
+            for symb in opration :
+                print (symb)
+        opration_symb = input('pick an opration from above : ')
+        print(f'{answear1} {opration_symb}')
+        num3 = float(input('what the third   number ? :'))
+        answear2 = calc(answear1,num3)
+        print(f'{answear1} {opration_symb} {num3} = {answear2}')
+        answear1 = answear2
+calculator()
