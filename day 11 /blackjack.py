faffe6f2dbae31c596a4de15bc82d3090cab3e12
mehrdad <meh1376@hotmail.com>
2022-08-09 08:57:22 +0430

    an other way to play blackjack
    an other way to play blackjack I could not perform well but it works

--- day 11 /blackjack.py
+++ day 11 /blackjack.py
@@ -1,5 +1,5 @@
+import os
 import random
-from traceback import print_tb
 ## The deck is unlimited in size. 
 ## There are no jokers. 
 ## The Jack/Queen/King all count as 10.
@@ -10,46 +10,64 @@ from traceback import print_tb
 ## Cards are not removed from the deck as they are drawn.
 ## The computer is the dealer.
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-pCards= []
-dCards= []
-pScore = 0 
-dScore = 0 
-
-def start_game (player_Cards,playes_score):
-    # couldn't add point to pScore ! so i just return it from func
+clear = lambda: os.system('clear')
+def game () :
+    pCards= []
+    dCards= []
+    pScore = 0 
+    dScore = 0 
     for i in range (2):
         point=random.choice(cards)
-        player_Cards.append(point)
-        playes_score+=sum(player_Cards)
-        return playes_score
-
-def take_a_card(player_Cards,player_score):
-    point=random.choice(cards)
-    player_Cards.append(point)
-    playes_score+=sum(player_Cards)
-    return playes_score
-
-
-pScore+=start_game(pCards,pScore)
-dScore+=start_game(dCards,dScore)
-
-print(f'dealer has {dCards} cards with point of {dScore}')
-print(f'player has {pCards} cards with point of {pScore}')
-
-if input('do you take an other card or not  y or n') =='y':
-    pScore+=take_a_card
-else:
+        pCards.append(point)
+        pScore+=point
+    for i in range (2):
+        point=random.choice(cards)
+        dCards.append(point)
+        dScore+=point
+    def score_board():
+        print(f'dealer has {dCards} cards with point of {dScore}')
+        print(f'player has {pCards} cards with point of {pScore}')
 
 
+    score_board()
 
-# for i in range (2):
-#     point=random.choice(cards)
-#     pCards.append(point)
-#     pScore+=point
+    while(True):
+        if pScore == 21 :
+            print('player won ! ')
+            score_board()
+            game ()
+            break
+        elif dScore == 21 :
+            print('dealer won ! ')
+            score_board()
+            game ()
+            break
+        elif pScore >21 :
+            print('player lose ! ')
+            score_board()
+            game ()
+            break
+        elif dScore >21 :
+            print('dealer lose ! ')
+            score_board()
+            game ()
+            break
 
-# for i in range (2):
-#     point=random.choice(cards)
-#     dCards.append(point)
-#     dScore+=point
-
-
+        elif dScore == pScore:
+            print('Draw ! ')
+            score_board()
+            game ()
+            break
+        else:
+            ask_user= input('do you take an other card or not  y or n')
+            if ask_user =='y':
+                point=random.choice(cards)
+                pCards.append(point)
+                pScore+=point
+                score_board()
+            elif ask_user =='n':
+                point=random.choice(cards)
+                dCards.append(point)
+                dScore+=point
+                score_board()
+game()
